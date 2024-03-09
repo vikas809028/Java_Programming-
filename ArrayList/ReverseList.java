788845bc6a12f9e4c49e2e9012929cb2413ac508
Vikas Tiwari <vikastiwari809028@gmail.com>
2024-03-09 21:37:12 +0530

    updated

--- ArrayList/ReverseList.java
+++ ArrayList/ReverseList.java
@@ -9,9 +9,25 @@ public class ReverseList {
         list.add(4);
         list.add(5);
         System.out.println(list);
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+        System.out.println(list);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        int n1 = list.size()-1;
+        int n2 = list1.size()-1;
+        
+            for(int i=n1,j=n2;j>=0; i--,j--){
+                list2.add(list.get(i)+list.get(j));
+            }
+            System.out.print(list2);
+        
 
-        for(int i=list.size()-1;i>=0; i--){
-            System.out.print(list.get(i)+ " ");
-        }
+            
+        
+        
     }
 }
